1b9f1644b40c7e13b0a9be956a3dd2724ec93206
bhupathiraju chalapathi naga varma <48557095+chalapathi444@users.noreply.github.com>
2020-12-17 21:35:04 +0530

    created ListView.builder.dart
    My practice for ListView.builder

---
+++ ListView.Builder.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List<String> elements = <String>['A', 'B', 'C'];
+  List<int> values = <int>[600, 300, 100];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List view"),
+      ),
+      body: Container(
+        height: 400,
+        child: ListView.builder(
+          itemBuilder: (BuildContext, int index) {
+            return Container(
+              height: 200,
+              child: Text('Entery ${elements[index]}'),
+              color: Colors.purple[values[index]],
+            );
+          },
+          itemCount: elements.length,
+        ),
+      ),
+    );
+  }
+}
